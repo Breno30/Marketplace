2a4effe517e631b23ed4aa14a1a1364b6e4f66b3
breno30 <brenodonascimentosilvaa@gmail.com>
2022-04-24 19:14:22 -0300

    Adding buttons

--- frontend/pagination.js
+++ frontend/pagination.js
@@ -3,9 +3,9 @@ getPage();
 $.ajax({
     type: 'GET',
     url: 'http://localhost:3000/posts/count'
-}).then(countPosts =>{
+}).then(countPosts => {
     const pageSize = 2;
-    const countPages = Math.ceil(countPosts/pageSize); 
+    const countPages = Math.ceil(countPosts / pageSize);
     drawPagination(countPages);
 });
 
@@ -13,9 +13,15 @@ function drawPagination(size) {
     const pagination = document.createElement('ul');
     pagination.className = 'pagination';
 
+    //draw "Previous"
+    pagination.appendChild(drawButtonPrevious());
+
     //draw every number
     for (number = 1; number <= size; number++)
         pagination.appendChild(drawNumber(number));
+
+    //draw "Next"
+    pagination.appendChild(drawButtonNext());
 
     document.getElementById('page-numbers').appendChild(pagination);
 }
@@ -28,9 +34,35 @@ function drawNumber(number) {
     pageLink.className = 'page-link';
     pageLink.innerText = number;
     pageLink.href = `index.html?page=${number}`
+
+
+    pageItem.appendChild(pageLink);
+    return pageItem;
+}
+
+function drawButtonNext() {
+    const pageItem = document.createElement('li');
+    pageItem.className = 'page-item';
+
+    const pageLink = document.createElement('a');
+    pageLink.className = 'page-link';
+    pageLink.innerText = 'Next';
+    pageLink.onclick = nextPage;
+
+    pageItem.appendChild(pageLink);
+    return pageItem;
+}
+
+function drawButtonPrevious() {
+    const pageItem = document.createElement('li');
+    pageItem.className = 'page-item';
+
+    const pageLink = document.createElement('a');
+    pageLink.className = 'page-link';
+    pageLink.innerText = 'Previous';
     pageLink.onclick = previousPage;
 
-
+    
     pageItem.appendChild(pageLink);
     return pageItem;
 }
