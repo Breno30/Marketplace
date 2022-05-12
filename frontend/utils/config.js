56428d1bd0d92a0a2d1395e7b9a9632664a455c5
breno30 <brenodonascimentosilvaa@gmail.com>
2022-05-12 16:37:36 -0300

    Adjusting wrong url

--- frontend/utils/config.js
+++ frontend/utils/config.js
@@ -1,4 +1,4 @@
 const config = {
     // api_url: "http://localhost:3000"
-    api_url: "https://marketplace30.herokuapp.com/" //change to your url
+    api_url: "https://marketplace30.herokuapp.com" //change to your url
 }
