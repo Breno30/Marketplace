f58077916d892610a23c044819a77ee963071e69
Breno30 <brenodonascimentosilvaa@gmail.com>
2023-01-08 12:58:15 -0300

    create transaction when creating order

--- backend/src/routes/orders.js
+++ backend/src/routes/orders.js
@@ -1,46 +1,103 @@
 const config = require('config');
-const auth = require('../middleware/auth')
+
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+
 router.use(express.json());
+const User = require('../models/user');
+const Post = require('../models/post');
 const Order = require('../models/order');
+const Transaction = require('../models/transaction');
 
 mongoose.connect(config.get('db'))
 
 router.get('/orders', (req, res) => {
-    const user = req.query.user
-    const product = req.query.product
+  const user = req.query.user
+  const product = req.query.product
 
-    const conditions = {}
-    if(user) conditions.userId = user
-    if(product) conditions.productId = product
+  const conditions = {}
+  if (user) conditions.userId = user
+  if (product) conditions.productId = product
 
-    Order
-        .find(conditions)
-        .then(result => res.send(result))
+  Order
+    .find(conditions)
+    .then(result => res.send(result))
 });
 
-router.get('/order/:id', (req,res) => {
-    const id = req.params.id.toString();
+router.get('/order/:id', (req, res) => {
+  const id = req.params.id.toString();
 
-    Order
-        .findById(id)
-        .then(result => res.send(result))
-        .catch(err => res.send(err.message));
+  Order
+    .findById(id)
+    .then(result => res.send(result))
+    .catch(err => res.send(err.message));
 })
 
-router.post('/order', (req, res) => {
-    const order = new Order({
-        userId: req.body.user_id,
-        productId: req.body.product_id,
-        paymentId: req.body.payment_id,
-        deliveryId: req.body.delivery_id
-    });
+router.post('/order', async (req, res) => {
+  const userId = req.body.user_id;
+  const productId = req.body.product_id;
 
-    order.save()
-        .then(res.send('Order successfully created!'))
-        .catch(err => res.send(err.message));
+  const user = await User.findById(userId);
+  const { name, email } = user;
+  
+  const product = await Post.findById(productId);
+  const { title, price } = product;
+
+  // Transaction
+  var data = JSON.stringify({
+    "transaction_amount": price,
+    "description": title,
+    "payment_method_id": "pix",
+    "payer": {
+      "email": email,
+      "first_name": name,
+      "last_name": "User",
+      "identification": {
+        "type": "phone",
+        "number": "551298940595"
+      },
+      "address": {
+        "zip_code": "06233200",
+        "street_name": "Av. das Nações Unidas",
+        "street_number": "3003",
+        "neighborhood": "Bonfim",
+        "city": "Osasco",
+        "federal_unit": "SP"
+      }
+    }
+  });
+
+  var config = {
+    method: 'post',
+    url: 'https://api.mercadopago.com/v1/payments',
+    headers: {
+      'Authorization': 'Bearer ' + process.env.Marketplace_MP_AUTH,
+      'Content-Type': 'application/json'
+    },
+    data: data
+  };
+
+  let response = await axios(config);
+  response = response.data;
+  const paymentId = response['id'];
+
+  let transaction = new Transaction({
+    type: 'pix',
+    status: 'pending'
+  });
+
+  transaction.save();
+
+  const order = new Order({
+    userId,
+    productId,
+    paymentId,
+    deliveryId: 'placehbolder'
+  });
+
+  order.save();
 });
 
 module.exports = router;
