45b33ab8c62414f7a5ce2f8fd146b07f7a9c02ec
Breno30 <brenodonascimentosilvaa@gmail.com>
2023-01-17 20:09:42 -0300

    extract transaction and delivery to methods

--- backend/src/models/transaction.js
+++ backend/src/models/transaction.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const Transaction = mongoose.model('Transaction', new mongoose.Schema({
+    mercadoPagoId: String,
     type: String,
     payment_date: Date,
     status: String
--- backend/src/routes/orders.js
+++ backend/src/routes/orders.js
@@ -12,7 +12,62 @@ const Delivery = require('../models/delivery');
 
 mongoose.connect(process.env.Marketplace_db);
 
-router.get('/orders', (req, res) => {
+const createTransaction = async (price, title, email, name) => {
+  const data = JSON.stringify({
+    "transaction_amount": price,
+    "description": title,
+    "payment_method_id": "pix",
+    "payer": {
+      "email": email,
+      "first_name": name,
+      "last_name": "User",
+      "identification": {
+        "type": "phone",
+        "number": "551298940595"
+      },
+      "address": {
+        "zip_code": "06233200",
+        "street_name": "Av. das Nações Unidas",
+        "street_number": "3003",
+        "neighborhood": "Bonfim",
+        "city": "Osasco",
+        "federal_unit": "SP"
+      }
+    }
+  });
+
+  const config = {
+    method: 'post',
+    url: 'https://api.mercadopago.com/v1/payments',
+    headers: {
+      'Authorization': 'Bearer ' + process.env.Marketplace_MP_AUTH,
+      'Content-Type': 'application/json'
+    },
+    data: data
+  };
+
+  const response = await axios(config);
+
+  let transaction = new Transaction({
+    mercadoPagoId: response.data._id,
+    type: 'pix',
+    status: 'pending',
+  });
+
+  const { _id } = await transaction.save();
+  return _id;
+}
+
+const createDelivery = async (starting_location, final_location) => {
+  const delivery = new Delivery({
+    starting_location,
+    final_location
+  });
+  const { _id } = await delivery.save();
+  return _id;
+}
+
+router.get('/orders', async (req, res) => {
   const user = req.query.user
   const product = req.query.product
 
@@ -45,58 +100,10 @@ router.post('/order', async (req, res) => {
   const { title, price, location } = product;
 
   // Transaction
-  var data = JSON.stringify({
-    "transaction_amount": price,
-    "description": title,
-    "payment_method_id": "pix",
-    "payer": {
-      "email": email,
-      "first_name": name,
-      "last_name": "User",
-      "identification": {
-        "type": "phone",
-        "number": "551298940595"
-      },
-      "address": {
-        "zip_code": "06233200",
-        "street_name": "Av. das Nações Unidas",
-        "street_number": "3003",
-        "neighborhood": "Bonfim",
-        "city": "Osasco",
-        "federal_unit": "SP"
-      }
-    }
-  });
-
-  var config = {
-    method: 'post',
-    url: 'https://api.mercadopago.com/v1/payments',
-    headers: {
-      'Authorization': 'Bearer ' + process.env.Marketplace_MP_AUTH,
-      'Content-Type': 'application/json'
-    },
-    data: data
-  };
-
-  let response = await axios(config);
-  response = response.data;
-  const paymentId = response['id'];
-
-  let transaction = new Transaction({
-    type: 'pix',
-    status: 'pending'
-  });
-
-  transaction.save();
+  const paymentId = await createTransaction(price, title, email, name);
 
   // Delivery
-  const delivery = new Delivery({
-    starting_location: location,
-    final_location: addresses[0].location
-  });
-
-  const { _id } = await delivery.save();
-  const deliveryId = _id;
+  const deliveryId = await createDelivery(location, addresses[0].location);
 
   // Order
   const order = new Order({
