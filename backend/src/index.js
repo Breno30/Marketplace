7e74f9eabf6e6e442bf08ce7130d291b54c16656
Breno30 <brenodonascimentosilvaa@gmail.com>
2023-04-11 19:51:31 -0300

    get body from form with bodyparser

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,9 @@ const express = require('express');
 const cors = require('cors');
 const cp = require('cookie-parser');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(cors({
